e39622742f27a22472b9d1ce080d976346f459c4
Mayank Sharma <mayanksharma250199@gmail.com>
2019-10-04 23:45:22 +0530

    Update app.service.ts

--- src/app/providers/app.service.ts
+++ src/app/providers/app.service.ts
@@ -4,7 +4,7 @@ import { Http } from '@angular/http';
 @Injectable()
 export class AppService {
 
-    url: string
+    url: any;
     constructor(private http : Http){
         this.url  = 'https://api.datamuse.com/words?ml=4'
     }
@@ -12,7 +12,7 @@ export class AppService {
     search_word(term){
         return this.http.get(this.url + term).map(res => {
             return res.json().map(item => {
-                return item.word
+                return item.word;
             })
         })
     }
